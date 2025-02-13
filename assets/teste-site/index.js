9b351e8dd7c778a99f1aef9e49f798ddc43456f4
Luísa Keys <luisa.chaves@intellissis.com.br>
2025-02-13 14:32:04 -0300

    atualizando portfolio 2025

---
+++ assets/teste-site/index.js
@@ -0,0 +1,24 @@
+const scrape = require('website-scraper');
+const PuppeteerPlugin = require('website-scraper-puppeteer');
+const path = require('path');
+
+scrape({
+    // Forneça a URL do site que você quer copiar
+    urls: ['https://zerobugs.com.br'],
+
+    // Especifique a pasta onde os arquivos do site serão salvos em pasta-do-site
+    directory: path.resolve(__dirname, 'C:\Users\luisa\Downloads\teste-site'),
+    
+    // carregue o plugin do Puppeteer
+    plugins: [ 
+        new PuppeteerPlugin({
+            launchOptions: { 
+                headless: true
+            },
+            scrollToBottom: {
+                timeout: 10000, 
+                viewportN: 10 
+            }
+        })
+    ]
+});
